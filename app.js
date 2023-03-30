621bcb39708c9347c79e747800aeed18f0f5517d
dlhx_liuwc <wencan.liu@dhc.com.cn>
2023-03-30 15:51:22 +0800

    增删改查接口

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = require("./router/router.js");
+// const bodyParser = require('body-parser')
 //创建web服务器 app接收
 const app = express();
+// app.use(bodyParser.urlencoded({ extended: false }))
 //配置跨域
 app.all("*",function(req,res,next){
   res.header("Access-Control-Allow-Origin", "*");
@@ -11,6 +13,7 @@ app.all("*",function(req,res,next){
   res.header("Content-Type", "application/json;charset=utf-8");
   next();//执行下一个中间件
 })
+app.use(express.urlencoded({extended:false})); //express 4/16版本之后可以直接使用
 app.use(router);
 app.listen(9090, () => {
   console.log("app listening on port 9090")
--- router/router.js
+++ router/router.js
@@ -2,14 +2,55 @@ const express = require("express");
 const conn = require('../mysql.js');
 const router = express.Router();
 //创建一个路由容器
-router.get('/getpost',(req,res)=>{
-  var sql = "select * from test1";
-  conn.query(sql,(error,result,fields)=>{
-    if(error) return "失败"
+router.get('/getpost', (req, res) => {
+  let sql = "select * from books";
+  conn.query(sql, (error, result) => {
+    if (error) return "查询失败"
     res.send({
-      msg:result
+      msg: result
     })
   })
 });
+
+router.post("/addbook", (req, res) => {
+  let id = req.body.id;
+  let name = req.body.name;
+  let createTime = new Date();
+  let sql = `insert into books(id,name,createTime) values(?,?,?)`;
+  conn.query(sql, [id, name, createTime], (error, result) => {
+    if (error) return "失败"
+    console.log("添加成功");
+    res.send({
+      msg: "添加成功"
+    })
+  })
+});
+
+router.delete("/delbook", (req, res) => {
+  let id = req.body.id;
+  let sql = "delete from books where id=?";
+  conn.query(sql, [id], (error, result) => {
+    if (error) return "失败"
+    console.log("删除成功");
+    res.send({
+      msg: "删除成功"
+    })
+  })
+})
+
+router.post("/updatebook", (req, res) => {
+  let id = req.body.id;
+  let name = req.body.name;
+  let updateTime = new Date();
+  let sql = "update books set name=? , updateTime=? where id=?";
+  conn.query(sql, [name, updateTime,id], (error, result) => {
+    if (error) return "失败"
+    console.log("更新成功");
+    res.send({
+      msg: "更新成功"
+    })
+  })
+})
+
 //导出
 module.exports = router;
